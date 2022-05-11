4a2c56de87bddb7205b0d353d907e8eba5cce881
James Houghton <jamesthornlea92@gmail.com>
2022-05-11 22:12:02 +0100

    Formatter for time

---
+++ internal/formatter.go
@@ -0,0 +1,29 @@
+package internal
+
+import "fmt"
+
+func FormatTotal(time int) string {
+	hours := time / 3600
+	minutes := (time % 3600) / 60
+	seconds := time % 60
+	totalString := ""
+	if hours > 0 {
+		totalString += fmt.Sprintf("%dh", hours)
+		if minutes > 0 {
+			totalString += " "
+		}
+	}
+
+	if minutes > 0 {
+		totalString += fmt.Sprintf("%dm", minutes)
+		if minutes > 0 {
+			totalString += " "
+		}
+	}
+
+	if seconds > 0 {
+		totalString += fmt.Sprintf("%ds", seconds)
+	}
+
+	return totalString
+}
