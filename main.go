06e700df7794673d357d3475709149199138160f
James Houghton <jamesthornlea92@gmail.com>
2022-06-08 22:05:11 +0100

    Update main

--- main.go
+++ main.go
@@ -1,19 +1,33 @@
 package main
 
+import (
+	"timetracker_cli/cmd"
+	"timetracker_cli/internal"
+)
+
+func setup(tracker *internal.Tracker) {
+	rootCmd := cmd.NewRootCmd(tracker)
+
+	listCmd := cmd.NewListCmd(tracker)
+	startCmd := cmd.NewStartCmd(tracker)
+	endCmd := cmd.NewEndCmd(tracker)
+	deletetCmd := cmd.NewDeleteCmd(tracker)
+
+	rootCmd.AddCommand(listCmd)
+	rootCmd.AddCommand(startCmd)
+	rootCmd.AddCommand(endCmd)
+	rootCmd.AddCommand(deletetCmd)
+
+	rootCmd.Execute()
+}
+
 func main() {
-	// start recording
+	config := internal.TrackerConfig{
+		File: "session.json",
+	}
 
-	// stop recording
+	tracker := internal.NewTracker(config)
+	defer tracker.Save()
 
-	// list times
-
-	// add time
-
-	// remove time
-
-	// clear all
-
-	// save
-
-	// restore
+	setup(tracker)
 }
